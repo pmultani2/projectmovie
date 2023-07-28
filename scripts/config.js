3d5eaa1295ee775322f10b45a75397939e7c2391
mpEmperor <multaniprabnoor71@gmail.com>
2023-07-28 13:19:11 -0400

    hide api key

--- scripts/config.js
+++ scripts/config.js
@@ -2,6 +2,6 @@ export const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzI3OTUyMWQ2OGZhMGU3ZmMwNzYyYWRkNTBkOWIyYSIsInN1YiI6IjY0Njg1YmY1MDA2YjAxMDBlNmI0MDY0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JXmoEYsUAEX4uHnSnIrzR5tTN0yg3lV2fau5Jc-g2nk'
+    Authorization: 'Bearer ' + process.env.API
   }
 };
